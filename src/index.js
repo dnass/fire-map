8cc9a332a28a85446f4c5a972550be49b2018462
Daniel Nass <dnlnss@gmail.com>
2021-07-15 10:43:04 -0400

    add GA

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GA4React from 'ga-4-react';
 import App from './App';
 import './index.css';
+
+new GA4React('G-SBVCST147G').initialize();
 
 ReactDOM.render(
   <React.StrictMode>
