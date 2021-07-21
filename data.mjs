5bee6a549df040a44b726ac3b1c8d045b862774b
Daniel Nass <dnlnss@gmail.com>
2021-07-21 10:13:01 -0500

    handle scrape errors

--- data.mjs
+++ data.mjs
@@ -2,39 +2,47 @@ import mapshaper from 'mapshaper';
 import fetch from 'node-fetch';
 import { writeFileSync } from 'fs';
 
-const files = ['progression.shp', 'progression.prj', 'progression.dbf'];
+try {
+  const files = ['progression.shp', 'progression.prj', 'progression.dbf'];
 
-const BASE_URL = 'https://cwfis.cfs.nrcan.gc.ca/downloads/hotspots/';
+  const BASE_URL = 'https://cwfis.cfs.nrcan.gc.ca/downloads/hotspots/';
 
-const data = await Promise.all(
-  files.map(file => fetch(`${BASE_URL}${file}`).then(res => res.buffer()))
-);
+  const data = await Promise.all(
+    files.map(file => fetch(`${BASE_URL}${file}`).then(res => res.buffer()))
+  );
 
-const input = files.reduce((obj, filename, i) => {
-  obj[filename] = data[i];
-  return obj;
-}, {});
+  const input = files.reduce((obj, filename, i) => {
+    obj[filename] = data[i];
+    return obj;
+  }, {});
 
-const cmd = `-i progression.shp -proj wgs84 -clean -each 'd = DATE + ""; this.properties = { date: [d.slice(0,4), d.slice(4,6), d.slice(6,8)].join("-"), area: AREA }' -o out.json format=topojson`;
+  const cmd = `-i progression.shp -proj wgs84 -clean -each 'd = DATE + ""; this.properties = { date: [d.slice(0,4), d.slice(4,6), d.slice(6,8)].join("-"), area: AREA }' -o out.json format=topojson`;
 
-const { 'out.json': output } = await mapshaper.applyCommands(cmd, input);
+  const { 'out.json': output } = await mapshaper.applyCommands(cmd, input);
 
-writeFileSync('src/data/perimeters.json', output);
+  writeFileSync('src/data/perimeters.json', output);
+} catch (err) {
+  console.log(err);
+}
 
-const ACTIVE_URL =
-  'https://cwfis.cfs.nrcan.gc.ca/geoserver/public/wfs?SERVICE=WFS&REQUEST=GetFeature&outputFormat=application%2Fjson&TYPENAME=public%3Aactivefires_current&cql_filter=hectares > 200';
+try {
+  const ACTIVE_URL =
+    'https://cwfis.cfs.nrcan.gc.ca/geoserver/public/wfs?SERVICE=WFS&REQUEST=GetFeature&outputFormat=application%2Fjson&TYPENAME=public%3Aactivefires_current&cql_filter=hectares > 200';
 
-const res = await fetch(ACTIVE_URL);
+  const res = await fetch(ACTIVE_URL);
 
-const { features } = await res.json();
+  const { features } = await res.json();
 
-const active = features
-  .filter(d => !['nrcc', 'nwcc', 'ak'].includes(d.properties.agency))
-  .sort((a, b) => b.properties.hectares - a.properties.hectares)
-  .map(d => ({
-    coordinates: [d.properties.lon, d.properties.lat],
-    name: d.properties.firename,
-    startDate: d.properties.startdate
-  }));
+  const active = features
+    .filter(d => !['nrcc', 'nwcc', 'ak'].includes(d.properties.agency))
+    .sort((a, b) => b.properties.hectares - a.properties.hectares)
+    .map(d => ({
+      coordinates: [d.properties.lon, d.properties.lat],
+      name: d.properties.firename,
+      startDate: d.properties.startdate
+    }));
 
-writeFileSync('src/data/active.json', JSON.stringify(active));
+  writeFileSync('src/data/active.json', JSON.stringify(active));
+} catch (err) {
+  console.log(err);
+}
